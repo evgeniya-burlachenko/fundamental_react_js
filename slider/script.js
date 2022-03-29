8491d7bd2334389920841a726b752ef5395e2080
evgeniya-burlachenko <e.accounted@gmail.com>
2022-03-29 16:41:21 +0300

    2

---
+++ slider/script.js
@@ -0,0 +1,43 @@
+const imgs = document.getElementById('imgs')
+
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
+
+const img = document.querySelectorAll('#imgs img')
+
+let ind = 0;
+
+let interval = setInterval(run, 2000)
+
+rightBtn.addEventListener('click', () => {
+	ind++
+	changeImg()
+	console.log(ind)
+})
+
+leftBtn.addEventListener('click', () => {
+	ind--
+	changeImg()
+	console.log(ind)
+})
+
+function changeImg(){
+	if(ind > img.length - 1) 
+	{
+		ind = 0
+	}
+	else if (ind < 0)
+	{
+		ind = img.length - 1
+	}
+	imgs.style.transform = `translateX(${-ind * 500}px)`
+}
+function resetInterval(){
+	clearInterval(interval)
+	interval = setInterval(run, 2000)
+}
+
+function run(){
+	ind++
+	changeImg()
+}
